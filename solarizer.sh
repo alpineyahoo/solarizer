0e2105fb25ca57cc00b45e7926b2541548a00e16
AlpineYahoo <101258543+alpineyahoo@users.noreply.github.com>
2024-10-01 07:36:44 +0900

    edit solarizer.sh

--- solarizer.sh
+++ solarizer.sh
@@ -9,4 +9,19 @@ grep Resolution |
 tr -dc '0-9x\n' |
 xargs -I _ magick -size _ "xc:#002b36" $HOME/.cache/solarizer/wallpaper.png # or "xc:#073642"
 
-osascript solarizer.applescript # > /dev/null 2>&1
+# osascript solarizer.applescript # > /dev/null 2>&1
+
+osascript << EOF > /dev/null 2>&1
+	try
+		with timeout of 0.1 seconds
+			tell application "Mission Control" to activate
+		end timeout
+	end try
+	tell application "System Events"
+		set x to (group 2 of group 1 of group 1 of process "Dock")
+		perform action 1 of button 1 of x
+		perform action 1 of last button of list 1 of x
+		delay 4 -- depends on how laggy your machine is
+		set picture of current desktop to POSIX file (do shell script "echo ~/.cache/solarizer/wallpaper.png")
+	end tell
+EOF
