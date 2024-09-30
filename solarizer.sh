17b0294bd5142b72285299a61767edacf8fb5cbc
AlpineYahoo <101258543+alpineyahoo@users.noreply.github.com>
2024-10-01 04:30:15 +0900

    edit solarizer.sh

--- solarizer.sh
+++ solarizer.sh
@@ -2,8 +2,11 @@
 
 # requires imagemagick
 
+cd "$(dirname "$(readlink -f "$0")")"
 mkdir -p $HOME/.cache/solarizer
 system_profiler SPDisplaysDataType |
 grep Resolution |
 tr -dc '0-9x\n' |
 xargs -I _ magick -size _ "xc:#002b36" $HOME/.cache/solarizer/wallpaper.png # or "xc:#073642"
+
+osascript solarizer.applescript > /dev/null 2>&1
