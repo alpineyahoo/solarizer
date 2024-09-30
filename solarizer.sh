daccad344ec99f5158a2a2ded46d2d5cf00e85dd
AlpineYahoo <101258543+alpineyahoo@users.noreply.github.com>
2024-10-01 04:37:47 +0900

    comment out in solarized.sh

--- solarizer.sh
+++ solarizer.sh
@@ -9,4 +9,4 @@ grep Resolution |
 tr -dc '0-9x\n' |
 xargs -I _ magick -size _ "xc:#002b36" $HOME/.cache/solarizer/wallpaper.png # or "xc:#073642"
 
-osascript solarizer.applescript > /dev/null 2>&1
+osascript solarizer.applescript # > /dev/null 2>&1
